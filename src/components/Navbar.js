a914541b28384312fdd140edf4f8268e7acbdb47
NachoRamon <99087089+NachoRamon@users.noreply.github.com>
2022-08-03 23:19:02 -0300

    Update and rename NavBar.js to Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import CartWidget from './CartWidget'
-
 const Navbar = () => {
   return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
